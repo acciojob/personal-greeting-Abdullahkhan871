0a51c4b32cb69964ea7c0e8dfb7e099fac87f11a
Abdullah khan <akhan283785@gmail.com>
2024-11-23 23:40:05 +0530

    first try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Greeting from "./Greeting";
 
 const App = () => {
+  const [displayValue, setDisplayValue] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Enter your name:</h1>
+      <input
+        type="texx"
+        onChange={(eve) => setDisplayValue(eve.target.value)}
+      />
+      {/* <p>{displayValue}</p>       */}
+      <Greeting props={displayValue} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
